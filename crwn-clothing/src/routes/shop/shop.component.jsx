5a6af6035e2e5ebf4f66a7868caed07cb26406c9
Antonov-S <anto1nov1@gmail.com>
2022-09-17 01:06:58 +0300

    adding shop component, adding hardcoded products data and product card component

---
+++ crwn-clothing/src/routes/shop/shop.component.jsx
@@ -0,0 +1,20 @@
+import { useContext } from "react";
+
+import { ProductsContext } from "../../components/context/products.context";
+import ProductCard from "../../components/product-card/productCard.component";
+
+import "./shop.styles.scss";
+
+const Shop = () => {
+    const { products } = useContext(ProductsContext);
+
+    return (
+        <div className="products-conteiner" >
+            {products.map((product) => (
+                <ProductCard key={product.id} product={product} />
+            ))};
+        </div>
+    );
+};
+
+export default Shop;
